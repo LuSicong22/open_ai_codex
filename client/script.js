b21c2874558b2c35aa54169964aa57c3cd4447cb
simon <simon.lu@tron.network>
2023-03-06 21:02:27 +0800

    c

--- client/script.js
+++ client/script.js
@@ -10,8 +10,10 @@ function loader(element) {
   element.textContent = "";
 
   loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
     element.textContent += ".";
 
+    // If the loading indicator has reached three dots, reset it
     if (element.textContent === "....") {
       element.textContent = "";
     }
@@ -31,21 +33,30 @@ function typeText(element, text) {
   }, 20);
 }
 
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
 function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16);
+
   return `id-${timestamp}-${hexadecimalString}`;
 }
 
 function chatStripe(isAi, value, uniqueId) {
   return `
-      <div class = "wrapper ${isAi && "ai"}">
-        <div class="chat">
-          <img src = "${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}"/>
+        <div class="wrapper ${isAi && "ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? "bot" : "user"}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
         </div>
-        <div class = "message" id=${uniqueId}>${value}</div>
-      </div>
     `;
 }
 
@@ -54,15 +65,49 @@ const handleSubmit = async (e) => {
 
   const data = new FormData(form);
 
+  // user's chatstripe
   chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
+  // to clear the textarea input
   form.reset();
 
+  // bot's chatstripe
   const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, "", uniqueId);
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // specific message div
   const messageDiv = document.getElementById(uniqueId);
+
+  // messageDiv.innerHTML = "..."
   loader(messageDiv);
+
+  const response = await fetch("http://localhost:5001/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
